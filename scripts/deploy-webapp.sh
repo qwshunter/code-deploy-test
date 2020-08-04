d6e6a0b7bc62d4f9b45cd9949b2c546ba264f2b2
Rahul Srivastava <qwsrahulsrivastava@gmail.com>
2020-08-04 20:59:20 +0530

    add

---
+++ scripts/deploy-webapp.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+REVISION=$1
+
+if [ -z "$REVISION"]; then
+	echo "No revision specified!"
+	exit 1
+fi
+
+STACK_OUTPUTS=`aws cloudformation describe-stacks --stack-name webapp-example --query "Stacks[0].Outputs[].[OutputValue]" --output text`
+DEPLOYMENT_BUCKET=`echo "$STACK_OUTPUTS" | grep webappdeploymentbucket`
+DEPLOYMENT_GROUP=`echo "$STACK_OUTPUTS" | grep WebappDeploymentGroup`
+APPLICATION_NAME=`echo "$STACK_OUTPUTS" | grep WebappApplication`
+
+aws deploy push --application-name $APPLICATION_NAME \
+	--s3-location s3://$DEPLOYMENT_BUCKET/$REVISION \
+	--source $DIR/../webapp > /dev/null
+
+aws deploy create-deployment --application-name $APPLICATION_NAME \
+	--s3-location bucket="$DEPLOYMENT_BUCKET",key="$REVISION",bundleType=zip \
+	--deployment-group-name $DEPLOYMENT_GROUP
